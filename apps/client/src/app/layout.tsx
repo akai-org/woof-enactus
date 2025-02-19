3b3049a19364ac2bcda6d903804dc831407abaa3
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-02-19 12:48:16 +0100

    refactor: remove unnecessary files on client

--- apps/client/src/app/layout.tsx
+++ apps/client/src/app/layout.tsx
@@ -1,17 +1,5 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import Providers from "./providers";
-import "./globals.css";
-
-const geistSans = Geist({
-	variable: "--font-geist-sans",
-	subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-	variable: "--font-geist-mono",
-	subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
 	title: "Create Next App",
@@ -28,7 +16,7 @@ export default function RootLayout({
 			lang="en"
 			suppressHydrationWarning
 		>
-			<body className={`${geistSans.variable} ${geistMono.variable}`}>
+			<body>
 				<Providers>{children}</Providers>
 			</body>
 		</html>
