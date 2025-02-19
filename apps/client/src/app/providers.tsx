f834387a6853331b314bd875792883d2db151de2
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-02-19 22:56:00 +0100

    refactor: use npm script to format apps/client code

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -1,6 +1,6 @@
-'use client';
+"use client";
 
-import { ChakraProvider, defaultSystem } from '@chakra-ui/react';
+import { ChakraProvider, defaultSystem } from "@chakra-ui/react";
 
 function Providers({ children }: { children: React.ReactNode }) {
   return <ChakraProvider value={defaultSystem}>{children}</ChakraProvider>;
