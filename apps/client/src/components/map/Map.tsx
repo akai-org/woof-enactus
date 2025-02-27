fdf24e04968da0244d885f1af3f7588c851ce5a2
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-02-27 19:15:55 +0100

    fix

--- apps/client/src/components/map/Map.tsx
+++ apps/client/src/components/map/Map.tsx
@@ -2,20 +2,28 @@
 
 import { Box } from "@chakra-ui/react";
 import L, { ErrorEvent, LocationEvent } from "leaflet";
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 
 function Map() {
+  const mapRef = useRef<L.Map | null>(null);
+
   useEffect(() => {
     const map = L.map("map", { zoomControl: false }).locate({
       setView: true,
     });
 
+    mapRef.current = map;
+
     const onLocationFound = (_: LocationEvent) => {
-      map.setZoom(12);
+      if (mapRef.current) {
+        mapRef.current.setZoom(12);
+      }
     };
 
     const onLocationError = (_: ErrorEvent) => {
-      map.setView([52.40379, 16.94935], 15);
+      if (mapRef.current) {
+        mapRef.current.setView([52.40379, 16.94935], 15);
+      }
     };
 
     map.once("locationfound", onLocationFound);
@@ -27,8 +35,10 @@ function Map() {
     }).addTo(map);
 
     return () => {
-      map.stopLocate();
-      map.remove();
+      if (mapRef.current) {
+        mapRef.current.stopLocate();
+        mapRef.current.remove();
+      }
     };
   }, []);
 
