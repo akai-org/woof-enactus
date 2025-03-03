e847c31df5672cfad4d35e7dd59a47169ad86453
YIlyaA <anukovich@gmail.com>
2025-03-03 22:40:03 +0100

    renaming markers on partners

--- apps/server/src/partners/partners.controller.ts
+++ apps/server/src/partners/partners.controller.ts
@@ -1,17 +1,17 @@
 import { Controller, Get, Param } from "@nestjs/common";
 import { PartnersService } from "./partners.service";
 
-@Controller("markers")
+@Controller("partners")
 export class PartnersController {
   constructor(private readonly partnersService: PartnersService) {}
 
-  // GET /markers
+  // GET /partners
   @Get()
   async getAllPartners() {
     return this.partnersService.findAll();
   }
 
-  // GET /markers/:uuid
+  // GET /partners/:uuid
   @Get(":uuid")
   async getPartnerByUuid(@Param("uuid") uuid: string) {
     return this.partnersService.findOne(uuid);
