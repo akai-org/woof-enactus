2d9dd22fd5c98b68825dd6ae8da352d47d8f4141
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-03-06 20:35:23 +0100

    feat: slider

--- apps/client/src/components/map/SearchBar.tsx
+++ apps/client/src/components/map/SearchBar.tsx
@@ -1,9 +1,11 @@
+"use client";
 import {
   Container,
   Input,
   Group,
   InputAddon,
-  Icon,
+  Slider,
+  useSlider,
   Button,
   Flex,
   Popover,
@@ -15,19 +17,55 @@ import {
   Text,
   Box,
   IconButton,
-  Editable,
 } from "@chakra-ui/react";
 import { FaSearch } from "react-icons/fa";
 import { IoClose } from "react-icons/io5";
 import { IoFilterSharp } from "react-icons/io5";
 
 export default function SearchBar() {
+  const slider = useSlider({
+    defaultValue: [0],
+    step: 10,
+    max: 100,
+  });
+
   return (
     <Container padding={4}>
       <Group attached width={"100%"}>
         <Input placeholder="Miasto, ulica" size="sm" />
         <InputAddon bg="none" size="sm">
-          +0km
+          <Popover.Root positioning={{ placement: "bottom-end" }}>
+            <Popover.Trigger padding={0} as="button">
+              + {slider.value}km
+            </Popover.Trigger>
+            <Portal>
+              <Popover.Positioner>
+                <Popover.Content>
+                  <Popover.Body>
+                    <Flex justify="space-between" align="center" gap={4}>
+                      <Slider.RootProvider value={slider} width="full">
+                        <Slider.Control>
+                          <Slider.Track>
+                            <Slider.Range />
+                          </Slider.Track>
+                          <Slider.Thumb index={0}>
+                            <Slider.HiddenInput />
+                          </Slider.Thumb>
+                        </Slider.Control>
+                      </Slider.RootProvider>
+                      <Popover.CloseTrigger
+                        fontWeight="medium"
+                        fontSize="2xl"
+                        cursor="pointer"
+                      >
+                        <IoClose />
+                      </Popover.CloseTrigger>
+                    </Flex>
+                  </Popover.Body>
+                </Popover.Content>
+              </Popover.Positioner>
+            </Portal>
+          </Popover.Root>
         </InputAddon>
         <InputAddon bg="none" padding={0}>
           <IconButton variant="ghost" size="xs">
