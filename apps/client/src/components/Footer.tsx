ff26a3c256c0000400b6aaaee8ff11425d93b4fe
Jakub Walkowiak <global-config@example.com>
2025-03-06 19:42:05 +0100

    fixing issues

---
+++ apps/client/src/components/Footer.tsx
@@ -0,0 +1,70 @@
+import { Box, Button, Flex, Icon, Text } from "@chakra-ui/react";
+import { Avatar } from "@chakra-ui/react";
+import { Tabs } from "@chakra-ui/react";
+import { Link } from "@chakra-ui/react";
+import { FiFacebook, FiInstagram, FiLinkedin } from "react-icons/fi";
+
+
+export const Footer = () => {
+    return (
+      <Box bg="palette.darker" p="25px" className="footer">
+        <Flex justify="space-between" align="center" position="relative">
+          {/* Lewa strona */}
+          <Text fontWeight="bold" color="palette.lighter">
+            @kontaktwithenactus@gmail.com
+          </Text>
+  
+          {/* Środkowy tekst, zawsze centrowany */}
+          <Text
+            fontWeight="bold"
+            color="palette.lighter"
+            position="absolute"
+            left="50%"
+            transform="translateX(-50%)"
+            whiteSpace="nowrap"
+          >
+            &#169; Enactus Poland
+          </Text>
+  
+          {/* Prawa strona - ikony */}
+          <Flex gap="10px">
+            <Link
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ boxShadow: "none", outline: "none" }}
+            >
+              <Icon color="palette.lighter">
+                <FiFacebook size="30px" />
+              </Icon>
+            </Link>
+  
+            <Link
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ boxShadow: "none", outline: "none" }}
+            >
+              <Icon color="palette.lighter">
+                <FiInstagram size="30px" />
+              </Icon>
+            </Link>
+  
+            <Link
+              href="https://linkedin.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              _hover={{ textDecoration: "none" }}
+              _focus={{ boxShadow: "none", outline: "none" }}
+            >
+              <Icon color="palette.lighter">
+                <FiLinkedin size="30px" />
+              </Icon>
+            </Link>
+          </Flex>
+        </Flex>
+      </Box>
+    );
+  };
