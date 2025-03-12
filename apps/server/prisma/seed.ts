09eb673921e3cd0b59512b56f6e66575a2b4d9a7
zakhildev <piotr.blecharski02@gmail.com>
2025-03-12 11:53:52 +0100

    feat: Update db seeder to new schema

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -1,36 +1,54 @@
-import { PrismaClient, Prisma } from '@prisma/client';
-import { faker } from '@faker-js/faker';
+import { PrismaClient, Prisma } from "@prisma/client";
+import { faker, fakerPL } from "@faker-js/faker";
 
 const prisma = new PrismaClient();
 
 // Insert 1000 records to test
 async function main() {
   const partnersData: Prisma.PartnerCreateManyInput[] = [];
+  const profilesData: Prisma.PartnerProfileCreateManyInput[] = [];
 
   for (let i = 0; i < 1000; i++) {
     partnersData.push({
+      id: i,
       name: faker.company.name(),
-      city: faker.location.city(),
-      street: faker.location.streetAddress(),
-      postal: faker.location.zipCode(),
-      phone: faker.phone.number(),
+      type: faker.helpers.arrayElement(["VET", "ORG", "SHOP", "SHELTER"]),
+    });
+    profilesData.push({
+      partnerId: i,
+      animals: faker.helpers.arrayElements(["Dogs", "Cats", "Parrots"], {
+        min: 1,
+        max: 3,
+      }),
+      city: fakerPL.location.city(),
+      street: fakerPL.location.street(),
+      getToInfo: "Jakieś tam instrukcje dojazdu...",
+      postal: fakerPL.location.zipCode("##-###"),
+      description: fakerPL.lorem.paragraph(),
+      visitHours: "Pon-Pt 10:30 - 14:00",
+      phone: fakerPL.phone.number(),
       website: faker.internet.url(),
-      type: faker.helpers.arrayElement(['VET', 'ORG', 'SHOP', 'SHELTER']),
     });
   }
 
-  const result = await prisma.partner.createMany({
+  const resultPartners = await prisma.partner.createMany({
     data: partnersData,
   });
 
-  console.log(`Inserted ${result.count} partners`);
+  const resultProfiles = await prisma.partnerProfile.createMany({
+    data: profilesData,
+  });
+
+  console.log(`Inserted ${resultPartners.count} partners`);
+  console.log(`Inserted ${resultProfiles.count} profiles`);
 }
 
 main()
-  .catch((e) => {
+  .catch(e => {
     console.error(e);
     process.exit(1);
   })
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
   .finally(async () => {
     await prisma.$disconnect();
   });
