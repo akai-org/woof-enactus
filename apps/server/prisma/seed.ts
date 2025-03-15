395eeb68998bdfe5c96629cc85ea2f9be735c261
zakhildev <piotr.blecharski02@gmail.com>
2025-03-15 18:01:07 +0100

    wip: working filters and similatrity

--- apps/server/prisma/seed.ts
+++ apps/server/prisma/seed.ts
@@ -30,7 +30,6 @@ async function main() {
       visitHours: "Pon-Pt 10:30 - 14:00",
       phone: fakerPL.phone.number(),
       website: faker.internet.url(),
-      type: faker.helpers.arrayElement(["VET", "ORG", "SHOP", "SHELTER"]),
     });
     hoursData.push({
       id: i,
