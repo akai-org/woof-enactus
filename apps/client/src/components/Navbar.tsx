bdad1bf16850ecafde8d88caa05198c13d29a83d
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-03-23 12:21:40 +0100

    refactor: update styling of Header and Footer to match updated theme

--- apps/client/src/components/Navbar.tsx
+++ apps/client/src/components/Navbar.tsx
@@ -7,13 +7,14 @@ import {
   IconButton,
   Portal,
   Text,
+  Container,
 } from "@chakra-ui/react";
 import { Avatar } from "@chakra-ui/react";
 import { Tabs } from "@chakra-ui/react";
 import { Link } from "@chakra-ui/react";
 import { IoMenu } from "react-icons/io5";
 
-const Links = [
+const links = [
   {
     name: "Mapa",
     href: "/",
@@ -32,134 +33,130 @@ const Links = [
   },
 ];
 
-export const Nav = () => {
+function Nav() {
   return (
-    <Tabs.Root
-      defaultValue="map"
-      display="flex"
-      justifyContent="space-between"
-      padding="20px"
-    >
-      <Flex gap={4} align="center">
-        <Drawer.Root placement="start" size="xs">
-          <Drawer.Trigger asChild>
-            <IconButton
-              size="sm"
-              bg="palette.darker"
-              hideFrom="md"
-              aria-label="Menu"
-            >
-              <IoMenu />
-            </IconButton>
-          </Drawer.Trigger>
-          <Portal>
-            <Drawer.Backdrop />
-            <Drawer.Positioner>
-              <Drawer.Content>
-                <Drawer.Header>
-                  <Flex justify="space-between" align="center">
-                    <Drawer.Title>
-                      <Text
-                        color="palette.lighter"
-                        fontSize="28px"
-                        fontWeight="bold"
-                      >
-                        Menu
-                      </Text>
-                    </Drawer.Title>
-                    <Drawer.CloseTrigger asChild>
-                      <CloseButton
-                        size="sm"
-                        border="solid 1px"
-                        borderColor="palette.darker"
-                      />
-                    </Drawer.CloseTrigger>
-                  </Flex>
-                </Drawer.Header>
-                <Drawer.Body display="flex" flexDirection="column" gap={8}>
-                  <For each={Links}>
-                    {(item, i) => (
-                      <Link
-                        href={item.href}
-                        _hover={{ textDecoration: "none" }}
-                        _focus={{ boxShadow: "none", outline: "none" }}
-                        fontSize="18px"
-                      >
-                        {item.name}
-                      </Link>
-                    )}
-                  </For>
-                </Drawer.Body>
-                <Drawer.Footer>
-                  <Text
-                    color="palette.lighter"
-                    fontSize="20px"
-                    fontWeight="bold"
+    <Tabs.Root defaultValue="map" paddingY="6">
+      <Container display="flex" justifyContent="space-between">
+        <Flex gap={4} align="center">
+          <Drawer.Root placement="start" size="xs">
+            <Drawer.Trigger asChild>
+              <IconButton
+                size="sm"
+                bg={"brand.700"}
+                hideFrom="md"
+                aria-label="Menu"
+                variant={"solid"}
+              >
+                <IoMenu />
+              </IconButton>
+            </Drawer.Trigger>
+            <Portal>
+              <Drawer.Backdrop />
+              <Drawer.Positioner>
+                <Drawer.Content>
+                  <Drawer.Header>
+                    <Flex justify="space-between" align="center">
+                      <Drawer.Title>
+                        <Text
+                          color="brand.500"
+                          textStyle={"3xl"}
+                          fontWeight="bold"
+                        >
+                          Menu
+                        </Text>
+                      </Drawer.Title>
+                      <Drawer.CloseTrigger asChild>
+                        <CloseButton size="xl" />
+                      </Drawer.CloseTrigger>
+                    </Flex>
+                  </Drawer.Header>
+                  <Drawer.Body display="flex" flexDirection="column" gap={8}>
+                    <For each={links}>
+                      {item => (
+                        <Link
+                          href={item.href}
+                          textDecoration="none"
+                          textStyle={"md"}
+                          _focus={{ boxShadow: "none", outline: "none" }}
+                        >
+                          {item.name}
+                        </Link>
+                      )}
+                    </For>
+                  </Drawer.Body>
+                  <Drawer.Footer>
+                    <Text
+                      color="brand.500"
+                      textStyle={"xl"}
+                      fontWeight="bold"
+                      textTransform={"uppercase"}
+                    >
+                      Haumaps
+                    </Text>
+                  </Drawer.Footer>
+                </Drawer.Content>
+              </Drawer.Positioner>
+            </Portal>
+          </Drawer.Root>
+          <Text
+            color="brand.500"
+            textStyle="2xl"
+            fontWeight="bold"
+            textTransform={"uppercase"}
+          >
+            Haumaps
+          </Text>
+        </Flex>
+
+        <Tabs.List
+          hideBelow="md"
+          defaultValue={links[0].name}
+          borderBottom="none"
+          minWidth="50%"
+          display="flex"
+          justifyContent="space-between"
+        >
+          <For each={links}>
+            {item => (
+              <Link
+                href={item.href}
+                textDecoration="none"
+                textStyle="md"
+                _focus={{ boxShadow: "none", outline: "none" }}
+              >
+                <Tabs.Trigger value={item.name} asChild>
+                  <Box
+                    color="brand.500"
+                    as="span"
+                    px="6"
+                    py="3"
+                    borderRadius="sm"
+                    transition="background-color 0.3s ease"
+                    _hover={{
+                      color: "brand.100",
+                      backgroundColor: "brand.500",
+                    }}
                   >
-                    HAUMAPS
-                  </Text>
-                </Drawer.Footer>
-              </Drawer.Content>
-            </Drawer.Positioner>
-          </Portal>
-        </Drawer.Root>
-        <Text color="palette.lighter" fontSize="28px" fontWeight="bold">
-          HAUMAPS
-        </Text>
-      </Flex>
+                    {item.name}
+                  </Box>
+                </Tabs.Trigger>
+              </Link>
+            )}
+          </For>
+        </Tabs.List>
 
-      <Tabs.List
-        hideBelow="md"
-        defaultValue={Links[0].name}
-        borderBottom="none"
-        minWidth="50%"
-        display="flex"
-        justifyContent="space-between"
-      >
-        <For each={Links}>
-          {(item, i) => (
-            <Link
-              href={item.href}
-              _hover={{ textDecoration: "none" }}
-              _focus={{ boxShadow: "none", outline: "none" }}
-            >
-              <Tabs.Trigger value={item.name} asChild>
-                <Box
-                  color="palette.lighter"
-                  as="button"
-                  px="20px" // Zwiększa obszar w prawo i lewo
-                  py="10px" // Zachowuje normalny padding góra/dół
-                  borderRadius="5px"
-                  transition="background-color 0.3s ease"
-                  _hover={{
-                    color: "white", // Tekst zmienia się na biały
-                    backgroundColor: "green",
-
-                    _before: { content: "none" }, // Ukrywa before (underline)
-                  }}
-                  _selected={{
-                    _before: {
-                      backgroundColor: "palette.lighter", // Zmiana koloru underline na aktywny
-                    },
-                  }}
-                >
-                  {item.name}
-                </Box>
-              </Tabs.Trigger>
-            </Link>
-          )}
-        </For>
-      </Tabs.List>
-
-      <Link
-        href="/login"
-        _hover={{ textDecoration: "none" }}
-        _focus={{ boxShadow: "none", outline: "none" }}
-      >
-        <Avatar.Root>
-          <Avatar.Fallback />
-        </Avatar.Root>
-      </Link>
+        <Link
+          href="/login"
+          textDecoration="none"
+          _focus={{ boxShadow: "none", outline: "none" }}
+        >
+          <Avatar.Root>
+            <Avatar.Fallback />
+          </Avatar.Root>
+        </Link>
+      </Container>
     </Tabs.Root>
   );
-};
+}
+
+export default Nav;
