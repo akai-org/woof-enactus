56d7b81e8502adb59682e5aa4ff7a8485d47d611
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-04-07 19:19:14 +0200

    fix(GoBackButton): go back in history instead of navigating to /

--- apps/client/src/components/ui/index.ts
+++ apps/client/src/components/ui/index.ts
@@ -1,5 +1,6 @@
 import Button from "./Button";
 import Link from "./Link";
 import Checkbox from "./Checkbox";
+import GoBackButton from "./GoBackButton";
 
-export { Button, Link, Checkbox };
+export { Button, Link, Checkbox, GoBackButton };
