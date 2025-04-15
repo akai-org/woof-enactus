4d1a5c89f4817a26711bf90fa10c860715174c0b
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-04-15 22:22:01 +0200

    feat: use note on client

--- apps/client/src/api/getPartnerNeeds.ts
+++ apps/client/src/api/getPartnerNeeds.ts
@@ -1,14 +1,14 @@
-import type { GenericServerResponse, PartnerNeed } from "@/types";
+import type { GenericServerResponse, PartnerNeeds } from "@/types";
 
 // TODO: add logger, improve error handling
 export default async function getPartnerNeeds(
   partnerSlug: string,
-): Promise<PartnerNeed[] | null> {
+): Promise<PartnerNeeds | null> {
   try {
     const res = await fetch(
       `${process.env.API_URL}/partners/${partnerSlug}/needed-goods`,
     );
-    const { data, ok }: GenericServerResponse<PartnerNeed[]> = await res.json();
+    const { data, ok }: GenericServerResponse<PartnerNeeds> = await res.json();
     if (!res.ok || !ok) {
       throw new Error();
     }
