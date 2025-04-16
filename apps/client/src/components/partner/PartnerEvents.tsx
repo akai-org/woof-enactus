3fcb5b7dda24b864247055b280019aaf2d8d474c
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-04-16 14:27:59 +0200

    feat: improve styling of partner events tab

--- apps/client/src/components/partner/PartnerEvents.tsx
+++ apps/client/src/components/partner/PartnerEvents.tsx
@@ -1,4 +1,4 @@
-import { Button, Card, Flex, Image, Stack } from "@chakra-ui/react";
+import { Box, Button, Card, Flex, Image, Stack } from "@chakra-ui/react";
 
 export default function PartnerEvents() {
   return (
@@ -8,23 +8,49 @@ export default function PartnerEvents() {
           display="grid"
           gridTemplateColumns={["1fr", "1fr", "2fr 1fr"]}
           gap={4}
-          p={[0, 4]}
+          p={{ base: 2, md: 4 }}
+          paddingBottom={{ base: "4", sm: 0 }}
           key={i}
+          borderColor="brand.300"
+          borderWidth={2}
+          placeItems="center"
         >
-          <div>
+          <Box>
             <Card.Header p={2} textAlign={["center", "left"]}>
-              <Card.Title color="brand.700" fontWeight="bold">
-                12.12.2000 - Lorem ipsum
+              <Card.Title
+                color="brand.600"
+                fontSize={{ base: "lg", md: "2xl" }}
+                fontWeight="bold"
+                mb={{ base: "2", md: "4" }}
+              >
+                12.07.2024 Zapraszamy na spacer z pieskami!
               </Card.Title>
             </Card.Header>
             <Card.Body p={2}>
-              <Card.Description>asdaskfjsdljgblsdhfgbl</Card.Description>
+              <Card.Description lineHeight={2}>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                condimentum in volutpat vel, dictum quis libero. Sed quis lorem
+                rhoncus, lobortis massa et, pellentesque risus. Fusce tempus
+                condimentum orci, vel suscipit nisi pellentesque sit amet.
+                Quisque volutpat mauris non aliquam sagittis. Maecenas eget enim
+                elementum, faucibus leo eu, volutpat lorem. Cras consequat
+                ligula ut blandit accumsan. Quisque pulvinar. Sed quis lorem
+                rhoncus, lobortis massa et, pellentesque risus.
+              </Card.Description>
             </Card.Body>
-          </div>
+          </Box>
 
-          <Flex direction="column" gap={4}>
+          <Flex direction="column" alignItems="center" gap={4}>
             <Image src="https://placehold.co/600x400/png" />
-            <Button variant="outline">Przypomnij mi o Wydarzeniu</Button>
+            <Button
+              paddingInline="4"
+              variant="solid"
+              backgroundColor="accent.gray"
+              color="brand.900"
+              fontWeight="semibold"
+            >
+              Przypomnij mi o wydarzeniu!
+            </Button>
           </Flex>
         </Card.Root>
       ))}
