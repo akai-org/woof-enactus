99eaefeba3aa724471159780de83359800a5c227
zakhildev <piotr.blecharski02@gmail.com>
2025-04-16 21:26:57 +0200

    fix: Incorrect value check

--- apps/server/src/auth/auth.service.ts
+++ apps/server/src/auth/auth.service.ts
@@ -51,7 +51,8 @@ export class AuthService {
   }
 
   async signUp(username: string, password: string): Promise<GenericResponse> {
-    const userExists = await this.accountsService.findOne(username);
+    const userExists = (await this.accountsService.findOne(username)).ok;
+    console.log(userExists);
     if (userExists) {
       const res: GenericResponse = {
         ok: false,
