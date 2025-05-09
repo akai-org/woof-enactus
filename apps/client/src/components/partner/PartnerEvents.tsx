3ac464b04f6c84741badf1cd718c700ccb8f9d98
Aleksander Nowicki <aleksander.nowicki04@gmail.com>
2025-05-09 20:16:55 +0200

    feat: fetch partner events from api

--- apps/client/src/components/partner/PartnerEvents.tsx
+++ apps/client/src/components/partner/PartnerEvents.tsx
@@ -1,16 +1,25 @@
+import { getPartnerEvents } from "@/api";
+import NotFound from "@/app/not-found";
 import { Box, Button, Card, Flex, Image, Stack } from "@chakra-ui/react";
 
-export default function PartnerEvents() {
+type Props = {
+  slug: string;
+};
+
+export default async function PartnerEvents({ slug }: Props) {
+   const events = await getPartnerEvents(slug);
+    if (!events) return NotFound();
+    
   return (
     <Stack gap={10}>
-      {Array.from({ length: 3 }).map((_, i) => (
+      {events.map(({title,description,thumbnail,eventDate,uuid}) => (
         <Card.Root
           display="grid"
           gridTemplateColumns={["1fr", "1fr", "2fr 1fr"]}
           gap={4}
           p={{ base: 2, md: 4 }}
           paddingBottom={{ base: "4", sm: 0 }}
-          key={i}
+          key={uuid}
           borderColor="brand.300"
           borderWidth={2}
           placeItems="center"
@@ -23,25 +32,18 @@ export default function PartnerEvents() {
                 fontWeight="bold"
                 mb={{ base: "2", md: "4" }}
               >
-                12.07.2024 Zapraszamy na spacer z pieskami!
+                {new Date(eventDate).toLocaleString().split(",")[0]}{" "}{title}
               </Card.Title>
             </Card.Header>
             <Card.Body p={2}>
               <Card.Description lineHeight={2}>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                condimentum in volutpat vel, dictum quis libero. Sed quis lorem
-                rhoncus, lobortis massa et, pellentesque risus. Fusce tempus
-                condimentum orci, vel suscipit nisi pellentesque sit amet.
-                Quisque volutpat mauris non aliquam sagittis. Maecenas eget enim
-                elementum, faucibus leo eu, volutpat lorem. Cras consequat
-                ligula ut blandit accumsan. Quisque pulvinar. Sed quis lorem
-                rhoncus, lobortis massa et, pellentesque risus.
+               {description}
               </Card.Description>
             </Card.Body>
           </Box>
 
           <Flex direction="column" alignItems="center" gap={4}>
-            <Image src="https://placehold.co/600x400/png" />
+            <Image src={thumbnail} alt={title} width={200} />
             <Button variant="gray" size="lg">
               Przypomnij mi o wydarzeniu!
             </Button>
