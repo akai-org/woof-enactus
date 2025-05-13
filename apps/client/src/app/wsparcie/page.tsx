0d1028d86499c8724e4f5e5d4e2293d46dafa965
Jakub Walkowiak <minilakeyt@gmail.com>
2025-05-13 13:21:00 +0200

    fix: table,support

--- apps/client/src/app/wsparcie/page.tsx
+++ apps/client/src/app/wsparcie/page.tsx
@@ -14,7 +14,7 @@ export default function SupportPage() {
   return (
     <Box backgroundSize="auto" bgColor="brand.100">
       <Container gap={20} display="flex" flexDirection="column" py={20}>
-        <Card.Root borderColor="black" padding="45px">
+        <Card.Root borderColor="black" padding="35px" hideBelow="md">
           <Card.Header>
             <Heading color="brand.500" size="5xl" fontWeight="600">
               Chcesz wesprzeć nasz projekt?
@@ -24,21 +24,8 @@ export default function SupportPage() {
             <div
               style={{ display: "flex", alignItems: "flex-start", gap: "20px" }}
             >
-              <div style={{ flex: 1, width: "50%", padding: "30px" }}>
+              <div style={{ flex: 1, width: "50%", padding: "20px" }}>
                 <Card.Description lineHeight="25px" fontWeight="500">
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce
-                  tempus condimentum orci, vel suscipit nisi pellentesque sit
-                  amet. Quisque volutpat mauris non aliquam sagittis. Maecenas
-                  eget enim elementum, faucibus leo eu, volutpat lorem. Cras
-                  consequat ligula ut blandit accumsan. Quisque pulvinar
-                  placerat neque, non fermentum nisl finibus a. Nunc rhoncus
-                  consectetur lectus. Praesent at orci dictum, egestas tortor
-                  sed, consequat ipsum. Seks ed eget leo kokaina nec kobiety
-                  metus blandit ultricies. Curabitur ut tellus lacus. Morbi
-                  lectus arcu, condimentum in volutpat vel, dictum quis libero.
-                  Sed quis lorem rhoncus, lobortis szybkie samochody massa et,
-                  pellentesque risus. Curabitur dapibus tellus eget sem commodo
-                  semper. Aenean ultricies nibh ipsum, quis ultrices justo
                   lacinia non. Sed dignissim tortor ac sapien volutpat pretium.
                   Donec tincidunt mauris risus, id interdum nulla consectetur
                   eget. Curabitur sit amet massa nec jestem gejem justo tempus
@@ -62,25 +49,84 @@ export default function SupportPage() {
                 padding="30px"
               />
             </div>
-            <Link href="https://ko-fi.com">
-              <Button 
-                margin="auto"
-                backgroundColor="accent.green"
-                padding="40px"
-                maxWidth="50%"
-                borderRadius="10px"
+            <Link
+              role="button"
+              margin="auto"
+              backgroundColor="accent.green"
+              padding="20px"
+              maxWidth="80%"
+              borderRadius="10px"
+              href="https://ko-fi.com"
+              textDecoration="none"
+            >
+              <IoBeer color="white" size="20px" />
+              <Heading
+                display="flex"
+                alignContent="center"
+                color="white"
+                size="3xl"
+                fontWeight="500"
               >
-                <IoBeer color="white" />
-                <Heading
-                  display="flex"
-                  alignContent="center"
-                  color="white"
-                  size="3xl"
-                  fontWeight="500"
-                >
-                  Support us on Ko-fi
-                </Heading>
-              </Button>
+                Support us on Ko-fi
+              </Heading>
+            </Link>
+          </Card.Body>
+        </Card.Root>
+
+        <Card.Root borderColor="black" padding="5px" hideFrom="md">
+          <Card.Header>
+            <Heading color="brand.500" size="5xl" fontWeight="600">
+              Chcesz wesprzeć nasz projekt?
+            </Heading>
+          </Card.Header>
+          <Card.Body>
+            <div style={{ display: "", alignItems: "flex-start", gap: "20px" }}>
+              <div style={{ flex: 1, width: "100%", padding: "10px" }}>
+                <Card.Description lineHeight="25px" fontWeight="500">
+                  lacinia non. Sed dignissim tortor ac sapien volutpat pretium.
+                  Donec tincidunt mauris risus, id interdum nulla consectetur
+                  eget. Curabitur sit amet massa nec jestem gejem justo tempus
+                  dignissim sed id lacus. Proin dignissim mi in nisi mollis, in
+                  imperdiet dui viverra. Integer tristique porttitor tristique.
+                  Nulla nec commodo turpis. Mauris vitae dictum est. Aenean a
+                  felis ac velit condimentum vestibulum id nec magna. Ut et
+                  velit id erat finibus porttitor at id sem. Sed vulputate
+                  tempor ex eget molestie. Etiam nibh ligula, blandit ornare
+                  magna laoreet, iaculis laoreet eros. Aliquam tempor
+                  sollicitudin iaculis. Praesent sit amet massa id felis mattis
+                  ultrices id faucibus eros. Sed et urna arcu. Integer ut elit
+                  sit amet magna vestibulum pulvinar. Fusce id lacus vitae odio
+                </Card.Description>
+              </div>
+              <Image
+                width="100%"
+                height="auto"
+                src={AboutUs1.src}
+                alt="Opis zdjęcia"
+                padding="30px"
+              />
+            </div>
+            <Link
+              role="button"
+              margin="auto"
+              backgroundColor="accent.green"
+              padding="20px"
+              maxWidth="80%"
+              borderRadius="10px"
+              href="https://ko-fi.com"
+              textDecoration="none"
+              textAlign="center"
+            >
+              <IoBeer color="white" size="30px" />
+              <Heading
+                display="flex"
+                alignContent="center"
+                color="white"
+                size="3xl"
+                fontWeight="500"
+              >
+                Support us on Ko-fi
+              </Heading>
             </Link>
           </Card.Body>
         </Card.Root>
--- apps/client/src/components/partners-table/Table.tsx
+++ apps/client/src/components/partners-table/Table.tsx
@@ -1,18 +1,24 @@
 "use client";
 
-import type { PartnerData } from "@/types";
+import type { PartnerData, PartnerType } from "@/types";
 import {
+  Accordion,
   Box,
   Button,
   ButtonGroup,
+  Flex,
+  HStack,
   IconButton,
   Pagination,
   Stack,
   Table,
+  Text,
 } from "@chakra-ui/react";
 import { useState } from "react";
 import { LuChevronLeft, LuChevronRight } from "react-icons/lu";
 import { IoReturnUpForward } from "react-icons/io5";
+
+import { Link } from "@/components";
 
 type MapProps = {
   data: PartnerData[];
@@ -25,83 +31,205 @@ export default function PartnersTable({ data }: MapProps) {
   const start = (page - 1) * itemsPerPage;
   const end = page * itemsPerPage;
 
+  const checkType = (type: string) => {
+    switch (type) {
+      case "VET":
+        return "#FFD600";
+      case "ORG":
+        return "brand.400";
+      case "SHOP":
+        return "brand.500";
+      case "SHELTER":
+        return "brand.700";
+    }
+  };
+
+  const checkName = (type: string) => {
+    switch (type) {
+      case "VET":
+        return "Weterynarz";
+      case "ORG":
+        return "Orgzanizacja prozwierzęca";
+      case "SHOP":
+        return "Sklep Zoologiczny";
+      case "SHELTER":
+        return "Schronisko";
+    }
+  };
+
   return (
-    <Stack fontSize="100px" fontFamily="body" fontWeight="400" width="full">
-
-      <Table.Root
-        width="auto"
-        size="sm"
-        variant="outline"
-        margin="20px"
-        borderRadius="5px"
-        marginTop="50px"
-        marginBottom="0px"
-      >
-        <Table.Header>
-          <Table.Row backgroundColor="white">
-            <Table.ColumnHeader color="brand.700">NAZWA</Table.ColumnHeader>
-            <Table.ColumnHeader color="brand.700">MIEJSCOWOŚĆ</Table.ColumnHeader>
-            <Table.ColumnHeader color="brand.700">ADRES</Table.ColumnHeader>
-            <Table.ColumnHeader color="brand.700">TELEFON</Table.ColumnHeader>
-            <Table.ColumnHeader color="brand.700">STRONA INTERNETOWA</Table.ColumnHeader>
-            <Table.ColumnHeader color="brand.700">KATEGORIA</Table.ColumnHeader>
-            <Table.ColumnHeader ></Table.ColumnHeader>
-          </Table.Row>
-        </Table.Header>
-        <Table.Body>
-          {data.slice(start, end).map((item, index) => (
-            <Table.Row
-              border="none"
-              height="80px"
-              key={item.id}
-              backgroundColor={index % 2 === 0 ? "brand.200" : "white"}
-            >
-              <Table.Cell>{item.name}</Table.Cell>
-              <Table.Cell>
-                {item.profile.postal} {item.profile.city}
-              </Table.Cell>
-              <Table.Cell>{item.profile.street}</Table.Cell>
-              <Table.Cell>{item.profile.phone}</Table.Cell>
-              <Table.Cell>{item.profile.website}</Table.Cell>
-              <Table.Cell>
-                <Stack direction="row" align="center">
-                  <Box
-                    boxSize="15px"
-                    borderRadius="sm"
-                    bg={
-                      item.type === "VET"
-                        ? "#FFD600"
-                        : item.type === "ORG"
-                          ? "brand.400"
-                          : item.type === "SHOP"
-                            ? "brand.500"
-                            : "brand.700"
-                    }
-                  />
-                  {item.type === "VET"
-                    ? "Weterynarz"
-                    : item.type === "ORG"
-                      ? "Orgzanizacja prozwierzęca"
-                      : item.type === "SHOP"
-                        ? "Sklep Zoologiczny"
-                        : "Schronisko"}
-                </Stack>
-              </Table.Cell>
-
-              <Table.Cell>
-                <Button backgroundColor="accent.green">
-                  SZCZEGÓŁY <IoReturnUpForward />
-                </Button>
-              </Table.Cell>
+    <Stack fontFamily="body" fontWeight="400" width="full">
+      <Flex direction="column" gap="2">
+        <Table.Root
+          width="auto"
+          size="sm"
+          variant="outline"
+          margin="20px"
+          borderRadius="5px"
+          marginTop="50px"
+          marginBottom="0px"
+          hideBelow="md"
+        >
+          <Table.Header>
+            <Table.Row backgroundColor="white">
+              <Table.ColumnHeader color="brand.700">NAZWA</Table.ColumnHeader>
+              <Table.ColumnHeader color="brand.700">
+                MIEJSCOWOŚĆ
+              </Table.ColumnHeader>
+              <Table.ColumnHeader color="brand.700">ADRES</Table.ColumnHeader>
+              <Table.ColumnHeader color="brand.700">TELEFON</Table.ColumnHeader>
+              <Table.ColumnHeader color="brand.700">
+                STRONA INTERNETOWA
+              </Table.ColumnHeader>
+              <Table.ColumnHeader color="brand.700">
+                KATEGORIA
+              </Table.ColumnHeader>
+              <Table.ColumnHeader></Table.ColumnHeader>
             </Table.Row>
+          </Table.Header>
+
+          <Table.Body>
+            {data.slice(start, end).map((item, index) => (
+              <Table.Row
+                border="none"
+                height="80px"
+                key={item.id}
+                backgroundColor={index % 2 === 0 ? "brand.200" : "white"}
+              >
+                <Table.Cell>{item.name}</Table.Cell>
+                <Table.Cell>
+                  {item.profile.postal} {item.profile.city}
+                </Table.Cell>
+                <Table.Cell minWidth="120px"> {item.profile.street}</Table.Cell>
+                <Table.Cell minWidth="120px">
+                  <Link
+                    linkProps={{ href: `tel:${item.profile.phone}` }}
+                    chakraLinkProps={{
+                      color: "black",
+                      textDecoration: "none",
+                      _focus: { boxShadow: "none", outline: "none" },
+                    }}
+                  >
+                    {item.profile.phone}
+                  </Link>
+                </Table.Cell>
+
+                <Table.Cell>
+                  <Link
+                    linkProps={{ href: item.profile.website ?? "" }}
+                    chakraLinkProps={{
+                      color: "black",
+                      textDecoration: "none",
+                      _focus: { boxShadow: "none", outline: "none" },
+                    }}
+                  >
+                    {item.profile.website}
+                  </Link>
+                </Table.Cell>
+                <Table.Cell>
+                  <Stack direction="row" align="center">
+                    <Box
+                      boxSize="15px"
+                      borderRadius="sm"
+                      bg={checkType(item.type)}
+                    />
+                    {checkName(item.type)}
+                  </Stack>
+                </Table.Cell>
+
+                <Table.Cell>
+                  <Link
+                    linkProps={{ href: `/placowki/${item.slug}` }}
+                    chakraLinkProps={{
+                      color: "brand.500",
+                      textDecoration: "none",
+                      _focus: { boxShadow: "none", outline: "none" },
+                    }}
+                  >
+                    <Button
+                      padding="20px"
+                      backgroundColor="accent.green"
+                      size="sm"
+                    >
+                      SZCZEGÓŁY <IoReturnUpForward />
+                    </Button>
+                  </Link>
+                </Table.Cell>
+              </Table.Row>
+            ))}
+          </Table.Body>
+        </Table.Root>
+
+        <Accordion.Root
+          collapsible
+          defaultValue={["0"]}
+          hideFrom="md"
+          borderWidth={1}
+          borderColor="brand.300"
+          borderRadius="sm"
+          mt="6"
+        >
+          {data.slice(start, end).map((item, index) => (
+            <Accordion.Item
+              key={item.id}
+              value={index.toString()}
+              bgColor={index % 2 === 0 ? "brand.200" : "white"}
+              py="2"
+            >
+              <Accordion.ItemTrigger justifyContent="space-between" px="4">
+                <HStack>
+                  <Text fontWeight="bold">{item.name}</Text>
+                </HStack>
+                <Accordion.ItemIndicator />
+              </Accordion.ItemTrigger>
+
+              <Accordion.ItemContent px="4" pb="4">
+                <Accordion.ItemBody>
+                  <Text>
+                    <strong>Miejscowość:</strong> {item.profile.postal}{" "}
+                    {item.profile.city}
+                  </Text>
+                  <Text>
+                    <strong>Adres:</strong> {item.profile.street}
+                  </Text>
+                  <Text>
+                    <strong>Telefon:</strong> {item.profile.phone}
+                  </Text>
+                  <Text>
+                    <strong>Strona WWW:</strong> {item.profile.website}
+                  </Text>
+                  <Flex align="center" gap={2} mt="2">
+                    <Box
+                      boxSize="15px"
+                      borderRadius="sm"
+                      bg={checkType(item.type)}
+                    />
+                    <Text fontSize="md">{checkName(item.type)}</Text>
+                  </Flex>
+
+                  <Link
+                    linkProps={{ href: `/placowki/${item.slug}` }}
+                    chakraLinkProps={{
+                      color: "brand.500",
+                      textDecoration: "none",
+                      _focus: { boxShadow: "none", outline: "none" },
+                    }}
+                  >
+                    <Button mt="4" backgroundColor="accent.green" size="sm">
+                      SZCZEGÓŁY <IoReturnUpForward />
+                    </Button>
+                  </Link>
+                </Accordion.ItemBody>
+              </Accordion.ItemContent>
+            </Accordion.Item>
           ))}
-        </Table.Body>
-      </Table.Root>
+        </Accordion.Root>
+      </Flex>
 
       <Pagination.Root
         margin="auto"
         marginBottom="20px"
-        marginTop="0px"
+        marginTop="20px"
         height="auto"
         count={data.length}
         pageSize={itemsPerPage}
