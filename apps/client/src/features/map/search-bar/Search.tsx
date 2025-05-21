6cebf691d42acf030a906b169c6c393f209c4ef1
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-05-21 14:12:17 +0200

    feat: searchbar clearbutton, form

--- apps/client/src/features/map/search-bar/Search.tsx
+++ apps/client/src/features/map/search-bar/Search.tsx
@@ -1,6 +1,12 @@
-import { Group, IconButton, Input } from "@chakra-ui/react";
+import {
+  CloseButton,
+  Group,
+  IconButton,
+  Input,
+  InputGroup,
+} from "@chakra-ui/react";
 import { useRouter, usePathname, useSearchParams } from "next/navigation";
-import { useCallback, useState } from "react";
+import { useCallback, useRef, useState, type FormEvent } from "react";
 import { FaLocationCrosshairs } from "react-icons/fa6";
 import { IoSearch } from "react-icons/io5";
 import { Tooltip } from "@/components";
@@ -18,65 +24,90 @@ export default function Search({ onLocate }: SearchProps) {
   const queryParam = searchParams.get(PARTNER_ADDRESS_SEARCH_PARAM) ?? "";
   const [value, setValue] = useState(queryParam);
 
-  const handleSearchSubmit = useCallback(() => {
-    const params = new URLSearchParams(searchParams);
-    const trimmedValue = value.trim();
+  const handleSearchSubmit = useCallback(
+    (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+      const params = new URLSearchParams(searchParams);
+      const trimmedValue = value.trim();
 
-    if (!trimmedValue) {
-      params.delete(PARTNER_ADDRESS_SEARCH_PARAM);
-    } else {
-      const parts = trimmedValue.includes(",")
-        ? trimmedValue.split(",")
-        : trimmedValue.split(" ");
+      if (!trimmedValue) {
+        params.delete(PARTNER_ADDRESS_SEARCH_PARAM);
+      } else {
+        const parts = trimmedValue.includes(",")
+          ? trimmedValue.split(",")
+          : trimmedValue.split(" ");
 
-      const cleanedParts = parts
-        .map(part => part.trim())
-        .filter(Boolean)
-        .slice(0, 2);
+        const cleanedParts = parts
+          .map(part => part.trim())
+          .filter(Boolean)
+          .slice(0, 2);
 
-      const queryValue = cleanedParts.join(",");
+        const queryValue = cleanedParts.join(",");
 
-      params.set(PARTNER_ADDRESS_SEARCH_PARAM, queryValue);
-    }
+        params.set(PARTNER_ADDRESS_SEARCH_PARAM, queryValue);
+      }
 
-    replace(`${pathname}?${params.toString()}`);
-  }, [searchParams, value, replace, pathname]);
+      replace(`${pathname}?${params.toString()}`);
+    },
+    [searchParams, value, replace, pathname],
+  );
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+
+  const endElement = value ? (
+    <CloseButton
+      size="xs"
+      onClick={() => {
+        setValue("");
+        inputRef.current?.focus();
+      }}
+      me="-2"
+    />
+  ) : undefined;
 
   return (
-    <Group w="full" marginRight={{ base: "0", md: "10%" }} marginY="2" attached>
-      <Input
-        placeholder="Miasto, ulica"
-        value={value}
-        onChange={e => {
-          setValue(e.currentTarget.value);
-        }}
-        borderColor="brand.300"
-        color="brand.700"
-        _placeholder={{ color: "brand.700" }}
-      />
-
-      <IconButton
-        variant="outline"
-        onClick={handleSearchSubmit}
-        borderLeft={0}
-        borderRight={0}
+    <form onSubmit={handleSearchSubmit} style={{width: "100%"}}>
+      <Group
+        w="full"
+        marginRight={{ base: "0", md: "10%" }}
+        marginY="2"
+        attached
       >
-        <IoSearch />
-      </IconButton>
-
-      <Tooltip
-        content="Zlokalizuj mnie"
-        positioning={{ placement: "right-end" }}
-      >
+        <InputGroup endElement={endElement}>
+          <Input
+            placeholder="Miasto, ulica"
+            value={value}
+            onChange={e => {
+              setValue(e.currentTarget.value);
+            }}
+            borderColor="brand.300"
+            color="brand.700"
+            _placeholder={{ color: "brand.700" }}
+          />
+        </InputGroup>
         <IconButton
-          onClick={onLocate}
           variant="outline"
-          borderTopLeftRadius={0}
-          borderBottomLeftRadius={0}
+          type="submit"
+          borderLeft={0}
+          borderRight={0}
         >
-          <FaLocationCrosshairs />
+          <IoSearch />
         </IconButton>
-      </Tooltip>
-    </Group>
+
+        <Tooltip
+          content="Zlokalizuj mnie"
+          positioning={{ placement: "right-end" }}
+        >
+          <IconButton
+            onClick={onLocate}
+            variant="outline"
+            borderTopLeftRadius={0}
+            borderBottomLeftRadius={0}
+          >
+            <FaLocationCrosshairs />
+          </IconButton>
+        </Tooltip>
+      </Group>
+    </form>
   );
 }
