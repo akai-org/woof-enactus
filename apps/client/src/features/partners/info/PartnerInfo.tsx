f988045ec4f51db23c6d78fb73be78b36741394b
Beniamin Szawracki <45177027+benqSzaw@users.noreply.github.com>
2025-06-04 12:57:05 +0200

    style: info tab style fix

--- apps/client/src/features/partners/info/PartnerInfo.tsx
+++ apps/client/src/features/partners/info/PartnerInfo.tsx
@@ -24,7 +24,7 @@ export default function PartnerInfo({ profileData }: PartnerInfoProps) {
 
   return (
     <Grid templateColumns={{ md: "repeat(2, 1fr)" }} gap={4} w="full">
-      <VStack gap={4} align="start">
+      <VStack gap={4}>
         <Card.Root w="full" borderColor="brand.300" borderWidth={2}>
           <Card.Header>
             <Flex align="center" gap={2} color="brand.700">
@@ -34,7 +34,7 @@ export default function PartnerInfo({ profileData }: PartnerInfoProps) {
               </Heading>
             </Flex>
           </Card.Header>
-          <Card.Body>
+          <Card.Body p="8px">
             <InfoBox title="Główny numer telefonu">
               <NullishGuard check={data.phone}>
                 <Link color="brand.900" href={`tel:${data.phone}`}>
